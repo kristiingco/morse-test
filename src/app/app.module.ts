6c46e239ba016a98fb0c9567557620659b2d8263
unknown <kristi.ingco@gmail.com>
2019-09-10 16:39:52 +0800

    Create button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartScreenComponent
+    StartScreenComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
