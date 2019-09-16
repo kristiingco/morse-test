3b0f7003170550143ee021040ae965084f208736
unknown <kristi.ingco@gmail.com>
2019-09-16 10:32:11 +0800

    Add routing to about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartScreenComponent},
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
