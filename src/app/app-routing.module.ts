ded86d79d1dde5f430c966638c7894de6db42ff3
unknown <kristi.ingco@gmail.com>
2019-09-17 15:52:47 +0800

    Route to input insructions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './about/about.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { InputInstructionsComponent } from './input-instructions/input-instructions.component';
 
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent},
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'input-instructions', component: InputInstructionsComponent }
 ];
 
 @NgModule({
