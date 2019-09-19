d8ffa7f9ec88261de2bf780c27e0d9876681e602
unknown <kristi.ingco@gmail.com>
2019-09-19 14:57:47 +0800

    Add route to input test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { InputInstructionsComponent } from './input-instructions/input-instructions.component';
+import { TestInputComponent } from './test-input/test-input.component';
 
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent},
   { path: 'about', component: AboutComponent },
-  { path: 'input-instructions', component: InputInstructionsComponent }
+  { path: 'input-instructions', component: InputInstructionsComponent },
+  { path: 'input-test', component: TestInputComponent }
 ];
 
 @NgModule({
