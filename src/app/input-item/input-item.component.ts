a319c2930670d2615689391757fff144be9c35a7
unknown <kristi.ingco@gmail.com>
2019-09-22 17:15:40 +0800

    Add multiple words

--- src/app/input-item/input-item.component.ts
+++ src/app/input-item/input-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-item',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputItemComponent implements OnInit {
 
+  @Input() visible: boolean;
+  numberOfItems = 1;
+  otherItems: Array<string> = ['EARTH', 'DINOS', 'STARE', 'TEASE', 'DARTS'];
+  word: string = this.otherItems[0];
+  score = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onKey(event) {
+    const inputValue = event.target.value;
+    for (let i = 0; i < inputValue.length; i++) {
+      if (inputValue[i] === this.word[i]) {
+        this.score += 1;
+      }
+    }
+    if (this.numberOfItems < 5) {
+      this.word = this.otherItems[this.numberOfItems];
+      this.numberOfItems++;
+    } else {
+      this.visible = false;
+    }
+
+    event.target.value = '';
   }
 
 }
