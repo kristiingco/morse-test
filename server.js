582a591f8ba8c11b145dc5d69951e5b8a5d727f4
Alyssa Cuan <alyssa.cuan@obf.ateneo.edu>
2019-10-08 17:01:29 +0800

    testing server api on heroku

--- server.js
+++ server.js
@@ -1,6 +1,53 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var QUESTIONS_COLLECTION = "question";
 
 var app = express();
+app.use(bodyParser.json());
 
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://salmon:sashimi1@ds331558.mlab.com:31558/heroku_www30lwf", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// MORSE API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+  
+  /*  "/api/questions"
+   *    GET: returns all question for a type
+   *    POST: creates a new contact
+   */
+  
+  app.get("/api/questions", function(req, res) {
+    db.collection(QUESTIONS_COLLECTION).find({}).toArray(function(err, docs) {
+        if (err) {
+          handleError(res, err.message, "Failed to get questions.");
+        } else {
+          res.status(200).json(docs);
+        }
+      });
+  });
