05c479bc510dc04d9d48c8de723734e336f7f50d
Alyssa Cuan <alyssa.cuan@obf.ateneo.edu>
2019-10-08 17:19:20 +0800

    added dist path to server

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var QUESTIONS_COLLECTION = "question";
 var app = express();
 app.use(bodyParser.json());
 
+var distDir = __dirname + "/dist/";
+ app.use(express.static(distDir));
+
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
