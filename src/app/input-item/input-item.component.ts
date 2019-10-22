6055eefe3f2ea63bc52fdf4f2dda028c7c31b663
unknown <kristi.ingco@gmail.com>
2019-10-22 15:17:17 +0800

    Revise input test

--- src/app/input-item/input-item.component.ts
+++ src/app/input-item/input-item.component.ts
@@ -2,6 +2,7 @@ declare var require: any;
 import { Component, OnInit, Input } from '@angular/core';
 import { QuestionService } from '../question.service';
 const morsify = require('morsify');
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-input-item',
@@ -13,7 +14,9 @@ export class InputItemComponent implements OnInit {
   @Input() visible: boolean;
   numberOfItems = 1;
   items = [];
-  word: string = this.items[0];
+  word: string;
+  currentLetter;
+  currentLetterIndex = 1;
   score = 0;
   showButton = false;
 
@@ -24,26 +27,88 @@ export class InputItemComponent implements OnInit {
       console.log(data);
       this.items = data;
       this.word = this.items[0].question;
+      this.currentLetter = this.word[0];
       console.log(this.word);
     });
+
+    $(document).ready(function() {
+      let lastKeyUpAt: Date = new Date();
+      let holdOnce = true;
+      let pressedDown = false;
+      let elem = $('input');
+
+      elem.on('keypress', function(e) {
+        if (e.keyCode === 32) {
+          elem.val(elem.val() + '.');
+          e.preventDefault();
+          return false;
+        }
+      });
+
+      elem.keydown(function(e) {
+        if (e.keyCode === 32) {
+        // Set key down time to the current time
+        let keyDownAt = new Date();
+
+        setTimeout(function() {
+            // Compare key down time with key up time
+            if (+keyDownAt > +lastKeyUpAt) {
+              if (holdOnce) {
+                elem.val(elem.val() + '-');
+                holdOnce = false;
+              }
+              pressedDown = true;
+              lastKeyUpAt = new Date();
+
+          }
+            // Key has been held down for x seconds
+          }, 600);
+
+        return false;
+
+        }
+    });
+
+      elem.keyup(function(e) {
+          // Set lastKeyUpAt to hold the time the last key up event was fired
+          if (e.keyCode === 32) {
+
+          if (pressedDown) {
+            pressedDown = false;
+            holdOnce = true;
+          } else {
+            elem.val(elem.val() + '.');
+          }
+
+          lastKeyUpAt = new Date();
+          return false;
+        }
+          });
+      });
   }
 
   onKey(event) {
     const inputValue = event.target.value;
     const decode = morsify.decode(inputValue);
-    for (let i = 0; i < decode.length; i++) {
-      if (decode[i] === this.word[i]) {
+    if (this.currentLetterIndex <= this.word.length) {
+      if (decode === this.currentLetter) {
         this.score += 1;
       }
-    }
-    if (this.numberOfItems < this.items.length) {
-      this.word = this.items[this.numberOfItems].question;
-      this.numberOfItems++;
-    } else {
-      this.visible = false;
-      this.showButton = true;
+      this.currentLetter = this.word[this.currentLetterIndex];
+      this.currentLetterIndex++;
     }
 
+    if (this.currentLetterIndex > this.word.length) {
+      if (this.numberOfItems < this.items.length) {
+        this.word = this.items[this.numberOfItems].question;
+        this.currentLetter = this.word[0];
+        this.currentLetterIndex = 1;
+        this.numberOfItems++;
+      } else {
+        this.visible = false;
+        this.showButton = true;
+      }
+    }
     event.target.value = '';
   }
 
