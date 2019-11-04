56644ec5ee07c75b5bec0e0e5901469ab84e990e
unknown <kristi.ingco@gmail.com>
2019-11-04 23:04:25 +0800

    Add score to database in input test

--- src/app/input-item/input-item.component.ts
+++ src/app/input-item/input-item.component.ts
@@ -1,6 +1,8 @@
 declare var require: any;
 import { Component, OnInit, Input } from '@angular/core';
 import { QuestionService } from '../question.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 const morsify = require('morsify');
 import * as $ from 'jquery';
 
@@ -19,8 +21,11 @@ export class InputItemComponent implements OnInit {
   currentLetterIndex = 1;
   score = 0;
   showButton = false;
+  answerString = '';
+  startTime = new Date().toLocaleTimeString();
+  endTime;
 
-  constructor(private questionService: QuestionService) { }
+  constructor(private questionService: QuestionService, private httpClient: HttpClient) { }
 
   ngOnInit() {
     this.questionService.getInputQuestions().subscribe((data: any[]) => {
@@ -94,16 +99,35 @@ export class InputItemComponent implements OnInit {
       if (decode === this.currentLetter) {
         this.score += 1;
       }
+      this.answerString += decode;
       this.currentLetter = this.word[this.currentLetterIndex];
       this.currentLetterIndex++;
     }
 
     if (this.currentLetterIndex > this.word.length) {
+      const data = {
+        question_id: this.items[this.numberOfItems - 1]._id,
+        user_id: 1, // alter after login api
+        score_obtained: this.score.toString(),
+        wrong_answer: (this.score !== this.word.length ? this.answerString : null),
+        start_timestamp: this.startTime,
+        end_timestamp: new Date().toLocaleTimeString()
+      };
+
+      this.startTime = new Date().toLocaleTimeString();
+
+      const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+
+      this.httpClient.post<any>('https://morse-test.herokuapp.com/api/scores', JSON.stringify(data), {headers})
+      .subscribe(res => console.log(res));
+
       if (this.numberOfItems < this.items.length) {
         this.word = this.items[this.numberOfItems].question;
         this.currentLetter = this.word[0];
         this.currentLetterIndex = 1;
         this.numberOfItems++;
+        this.answerString = '';
+        this.score = 0;
       } else {
         this.visible = false;
         this.showButton = true;
