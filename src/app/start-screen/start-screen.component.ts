503a7ed73e9911fbc283815df86b04170cb8d07a
unknown <kristi.ingco@gmail.com>
2019-11-19 17:24:02 +0800

    Add login support

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-start-screen',
@@ -8,13 +10,39 @@ import { Router } from '@angular/router';
 })
 export class StartScreenComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  userId = new FormControl('');
+  password = new FormControl('');
+
+  constructor(private router: Router, private httpClient: HttpClient) { }
 
   ngOnInit() {
+    if (localStorage.getItem('user_id')) {
+      this.router.navigate(['/input-instructions']);
+    }
   }
 
   goToPage(pageName: string): void {
     this.router.navigate(['${pageName}']);
+  }
+
+  logIn() {
+
+    const data = {
+      user_id: this.userId.value,
+      password: this.password.value
+    };
+
+    console.log(data);
+
+    const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+
+    this.httpClient.post<any>('https://morse-test.herokuapp.com/api/login', JSON.stringify(data), {headers})
+      .subscribe(res => {
+        if (res) {
+          this.router.navigate(['/input-instructions']);
+          localStorage.setItem('user_id', this.userId.value);
+        }
+      });
   }
 
 }
