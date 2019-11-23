63d86666cad337765569dc2471c99e947eb8dbc0
unknown <kristi.ingco@gmail.com>
2019-11-23 19:26:52 +0800

    Test

--- server.js
+++ server.js
@@ -113,6 +113,7 @@ function handleError(res, reason, message, code) {
             if (doc.length === 0) {
                 console.log("User doesn't exist");
             } 
+        }
     });
       if (userObject.password == password){
         //search scores collection for the latest question the user answered
